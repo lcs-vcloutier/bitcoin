b438239f158ae217e152323aef5bdca61ec366eb
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-13 13:49:39 -0400

    connected view model

--- bitcoin/Feature/Coins/Screen/CoinsScreen.swift
+++ bitcoin/Feature/Coins/Screen/CoinsScreen.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct CoinsScreen: View {
+    // Injecting service into viewmodel
+    @StateObject private var vm = CoinsViewModelImpl(service: CoinsServiceImpl())
+
     var body: some View {
         NavigationView {
-            List(Coin.dummyData, id: \.symbol) { item in
+            List(vm.coins, id: \.symbol) { item in
                 CoinView(item: item)
             }
             .navigationBarTitle("Cryptos")
+        }
+        .task {
+            // get the coins when needed
+            await vm.getCoins()
         }
     }
 }
