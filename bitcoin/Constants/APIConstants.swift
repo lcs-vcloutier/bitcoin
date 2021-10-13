61fe70c49ff9125c668fc346f8f881564cd8ca5b
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-12 21:33:41 -0400

    defined base url

---
+++ bitcoin/Constants/APIConstants.swift
@@ -0,0 +1,13 @@
+//
+//  APIConstants.swift
+//  bitcoin
+//
+//  Created by Vincent Cloutier on 2021-10-12.
+//
+
+import Foundation
+
+// Define the URL that doesn't change
+enum APIConstants {
+    static let baseURL = "https://api.coinlore.net/api"
+}
