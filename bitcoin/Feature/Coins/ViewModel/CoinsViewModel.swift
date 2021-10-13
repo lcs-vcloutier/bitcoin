d48a5e68d67ff41ee4ead018430c51471db47ae5
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-13 13:43:51 -0400

    added view model  created the  array to store the coins

---
+++ bitcoin/Feature/Coins/ViewModel/CoinsViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  CoinsViewModel.swift
+//  bitcoin
+//
+//  Created by Vincent Cloutier on 2021-10-13.
+//
+
+import Foundation
+
+
+// Interact with service class and handle any additional business logic
+
+protocol CoinsViewModel: ObservableObject {
+    func getCoins() async
+}
+
+@MainActor // You need this w/ async await to signify you want to update this on the main thread
+final class CoinsViewModelImpl: CoinsViewModel {
+    
+    
+    @Published private(set) var coins: [Coin] = [] // access from outside can't change from outside
+    
+    private let service: CoinsService
+    
+    init(service: CoinsService) {
+        self.service = service
+    }
+    func getCoins() async {
+        do {
+            self.coins = try await service.fetchCoins()
+        } catch {
+            print(error)
+        }
+    }
+}
