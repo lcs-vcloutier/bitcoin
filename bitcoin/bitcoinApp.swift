07a5e375655458f557b4bf5895111f30cf623fdd
Vincent Cloutier <vcloutier@lcs.on.ca>
2021-10-12 21:29:48 -0400

    Initial Commit

---
+++ bitcoin/bitcoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitcoinApp.swift
+//  bitcoin
+//
+//  Created by Vincent Cloutier on 2021-10-12.
+//
+
+import SwiftUI
+
+@main
+struct bitcoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
